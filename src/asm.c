cc6382340cd3d5d3c1718b5b3010aaed37af3b42
John Powell <john_powell@partech.com>
2024-07-26 16:41:28 -0400

    Simple assembler implementation added

--- src/asm.c
+++ src/asm.c
@@ -1,10 +1,45 @@
 #include "evm/opcodes.h"
+#include "evm/asm.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  int result = EXIT_SUCCESS;
+  evm_assembler_t *assembler = evmasmInitialize(evmasmAllocate());
+  int result = EXIT_FAILURE;
+  int arg;
+
+  if(assembler) {
+    for(result = EXIT_SUCCESS, arg = 1; result == EXIT_SUCCESS && arg < argc; ++arg) {
+      FILE *src = fopen(argv[arg], "r");
+
+      if(!src) {
+        fprintf(stderr, "%s: failed to open %s for reading\n", *argv, argv[arg]);
+        result = EXIT_FAILURE;
+      }
+      else if(evmasmParseFile(assembler, argv[arg], src)) {
+        fprintf(stderr, "%s: failed to successfully parse %s\n", *argv, argv[arg]);
+        result = EXIT_FAILURE;
+      }
+
+      fclose(src);
+    }
+
+    if(result == EXIT_SUCCESS) {
+      if(!evmasmValidateProgram(assembler)) {
+        if(evmasmProgramToFile(assembler, stdout)) {
+          fprintf(stderr, "%s: program failed to output\n", *argv);
+          result = EXIT_FAILURE;
+        }
+      }
+      else {
+        fprintf(stderr, "%s: program failed to validate\n", *argv);
+        result = EXIT_FAILURE;
+      }
+    }
+
+    evmasmFree(evmasmFinalize(assembler));
+  }
 
   return result;
 }

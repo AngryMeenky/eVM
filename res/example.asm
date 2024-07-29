3637ca42efdc6b7dd1273130bc3736ccae7c902d
John Powell <john_powell@partech.com>
2024-07-29 17:15:57 -0400

    Updated to leave the stack empty

--- res/example.asm
+++ res/example.asm
@@ -8,9 +8,10 @@ function:
   BLTIN 0 ; generate program checksum
   BLTIN 1 ; dump stack to stdout
   BLTIN 2 ; dump the program to stdout
-  RET
+  RET   1 ; use the return address 1 down from the top of the stack
 
 entry:
   CALL function
+  POP
   HALT
 

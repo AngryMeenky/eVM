f96128c8e1c861a5846bbbb0d52e0fdef88ff0a0
John Powell <john_powell@partech.com>
2024-08-02 15:37:03 -0400

    Added assembler/disassembler test files
    * no_float uses every non floating point instruction
    * yes_float uses every instruction, including all floating point
      instructions
    * There is a sizable unreachable region in each file allowing for
      execution in the example binding executable.

---
+++ res/no_float.asm
@@ -0,0 +1,123 @@
+; simple example assembly file for eVM
+.name MAIN
+.offset 0
+
+entry:
+  NOP
+  PUSH 100000000 ; five byte push
+  DUP            ; duplicate the top value in the stack
+  ADD            ; pop the top two values and push the result of addition
+  PUSH 0         ; single byte push
+  SUB            ; pop the top two values and push the result of subtraction
+  ABS            ; replace the top of the stack with its absolute value
+  PUSH 8192      ; three byte push
+  SWAP           ; swap the top two stack values
+  PUSH -1        ; single byte push
+  PUSH 63        ; two byte push
+  MUL            ; pop the top two values and push the result of multiplication
+  PUSH 1         ; single byte push
+  SWAP           ; swap the top two stack values
+  DIV            ; pop the top two values and push the result of division
+  NEG            ; replace the top of the stack with its negated value
+  PUSH 86400     ; four byte push
+  INC            ; increment the top of the stack
+  DEC            ; decrement the top of the stack
+  CALL report
+  POP 5          ; empty the stack
+  BLTIN 1        ; dump stack to stdout
+  HALT
+
+unreachable:
+  DUP  2         ; duplicate the second value in the stack
+  DUP  3         ; duplicate the third value in the stack
+  DUP  4         ; duplicate the fourth value in the stack
+  DUP  5         ; duplicate the fifth value in the stack
+  DUP  6         ; duplicate the sixth value in the stack
+  DUP  7         ; duplicate the seventh value in the stack
+  DUP  8         ; duplicate the eighth value in the stack
+  DUP  9         ; duplicate the ninth value in the stack
+  DUP 10         ; duplicate the tenth value in the stack
+  DUP 11         ; duplicate the eleventh value in the stack
+  DUP 12         ; duplicate the twelfth value in the stack
+  DUP 13         ; duplicate the thirteenth value in the stack
+  DUP 14         ; duplicate the fourteenth value in the stack
+  DUP 15         ; duplicate the fifteenth value in the stack
+  DUP 16         ; duplicate the sixteenth value in the stack
+  JTBL
+.addr unreachable
+.addr report
+.addr entry
+.addr returns
+  POP            ; pop the top value from the stack
+  POP 2          ; pop the top two values from the stack
+  POP 3          ; pop the top three values from the stack
+  POP 4          ; pop the top four values from the stack
+  POP 5          ; pop the top five values from the stack
+  POP 6          ; pop the top six values from the stack
+  POP 7          ; pop the top seven values from the stack
+  POP 8          ; pop the top eight values from the stack
+  LJTBL
+.addr unreachable
+.addr report
+.addr entry
+.addr returns
+  REM 1          ; remove the second value from the stack
+  REM 2          ; remove the third value from the stack
+  REM 3          ; remove the fourth value from the stack
+  REM 4          ; remove the fifth value from the stack
+  REM 5          ; remove the sixth value from the stack
+  REM 6          ; remove the seventh value from the stack
+  REM 7          ; remove the eighth value from the stack
+  REM 12 3       
+  LSH
+  RSH
+  AND
+  OR
+  XOR
+  INV
+  BOOL
+  NOT
+  CMP -1
+  CMP  0
+  CMP  1
+  CMP
+  JMP unreachable
+  JLT unreachable
+  JLE unreachable
+  JNE unreachable
+  JEQ unreachable
+  JGE unreachable
+  JGT unreachable
+  LJMP unreachable
+  LJLT unreachable
+  LJLE unreachable
+  LJNE unreachable
+  LJEQ unreachable
+  LJGE unreachable
+  LJGT unreachable
+
+returns:
+  RET
+  RET  1
+  RET  2
+  RET  3
+  RET  4
+  RET  5
+  RET  6
+  RET  7
+  RET  8
+  RET  9
+  RET 10
+  RET 11
+  RET 12
+  RET 13
+  RET 14
+  RET 144
+  
+
+report:
+  BLTIN 0 ; generate program checksum and push it onto the stack
+  BLTIN 1 ; dump stack to stdout
+  BLTIN 2 ; dump the program to stdout
+  RET   1 ; use the return address 1 down from the top of the stack
+

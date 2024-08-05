d6a0c88b2746b20b98ff016b49671e19d5ecd7bd
John Powell <john_powell@partech.com>
2024-08-05 12:41:41 -0400

    Added eVM configuration for gdextension

---
+++ gdext/inc/extcfg.h
@@ -0,0 +1,12 @@
+#ifndef GDEXT_EVM_CONFIG_H
+#  define GDEXT_EVM_CONFIG_H
+
+// enable float support
+#define EVM_FLOAT_SUPPORT (1)
+// allow a full compliment of builtin functions
+#define EVM_MAX_BUILTINS (256)
+// allow extenal allocation of the stack
+#define EVM_STATIC_STACK (1)
+
+#endif /* GDEXT_EVM_CONFIG_H */
+

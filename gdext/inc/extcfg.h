582ae63ad551f9dc5622d4f12c266993f8dc2b24
John Powell <Powell_John@bah.com>
2026-09-08 10:56:26 -0400

    Comments reflect the actual configuration values

--- gdext/inc/extcfg.h
+++ gdext/inc/extcfg.h
@@ -5,8 +5,12 @@
 #define EVM_FLOAT_SUPPORT (1)
 // allow a full compliment of builtin functions
 #define EVM_MAX_BUILTINS (256)
-// allow extenal allocation of the stack
+// statically allocate the stack
 #define EVM_STATIC_STACK (1)
+// dynamically allocate main memory
+#define EVM_STATIC_MEMORY (0)
+// dynamically allocate program memory
+#define EVM_STATIC_PROGRAM (0)
 // only print internal eVM errors
 #define EVM_LOG_LEVEL (2)
 
